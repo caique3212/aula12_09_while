b2efeb48ca01a465747405fd7790a24af73a5c1e
caique coelho <caiqueramos.2240@aluno.saojudas.br>
2023-09-12 21:40:37 -0300

    pares1a1000

---
+++ LeNumerosAte0.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class LeNumerosAte0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int soma = 0;
+        int cont = 0;
+        
+        System.out.println("digite seus numeros, 0 encerra ");
+        int n = scanner.nextInt();
+        while (n != 0 ){
+            soma = soma + n;
+            cont = cont + 1;
+            System.out.print("proximo: ");
+            n = scanner.nextInt();
+        }
+        
+        System.out.println("soma dos valores: " + soma);
+        if (cont == 0) {
+            System.out.println("o valor foi digitado");
+        }
+        
+        else {
+            double media = (double)soma / cont;
+            System.out.println("media dos valores: " + media);
+        }
+
+
+        
+        
+
+        scanner.close();
+        
+    }
+}
